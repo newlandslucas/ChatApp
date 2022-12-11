e507dc9f5b6da7a13f335afd62f47af4950a8885
C1326386 Lucas Silveira Newlands Machado <lucas.newlands@ibm.com>
2022-12-11 20:16:36 -0300

    Start project

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Lucas Newlands on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
